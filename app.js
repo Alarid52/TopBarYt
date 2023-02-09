0331c2d88b7e89fe5050d5b6b67bce75bfebe28b
Alarid52 <alarid152@gmail.com>
2023-02-09 11:34:31 -0600

    se añade scrollbar avatar-menu

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ const videoMenu = document.querySelector('.video-menu');
 const btnNotification = document.querySelector('#btn-notification');
 const notificationMenu = document.querySelector('.notification-menu');
 
-btnAvatar.addEventListener('click', ()=>{
+btnAvatar.addEventListener('click', () => {
     avatarMenu.classList.toggle('active');
 });
 
-btnVideo.addEventListener('click', ()=>{
+btnVideo.addEventListener('click', () => {
     videoMenu.classList.toggle('active');
 });
 
-btnNotification.addEventListener('click', ()=>{
+btnNotification.addEventListener('click', () => {
     notificationMenu.classList.toggle('active');
 });
