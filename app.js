0037454b5cc186f9c9bca9db0e0b406b5aa64f35
Alarid52 <alarid152@gmail.com>
2023-02-09 10:58:25 -0600

    Agrega menu-video y menu-notificaciones

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const btnAvatar = document.querySelector('#avatar');
-const menu = document.querySelector('.avatar-menu');
+const avatarMenu = document.querySelector('.avatar-menu');
+
+const btnVideo = document.querySelector('#btn-yt');
+const videoMenu = document.querySelector('.video-menu');
+
+const btnNotification = document.querySelector('#btn-notification');
+const notificationMenu = document.querySelector('.notification-menu');
 
 btnAvatar.addEventListener('click', ()=>{
-    menu.classList.toggle('active');
+    avatarMenu.classList.toggle('active');
+});
+
+btnVideo.addEventListener('click', ()=>{
+    videoMenu.classList.toggle('active');
+});
+
+btnNotification.addEventListener('click', ()=>{
+    notificationMenu.classList.toggle('active');
 });
